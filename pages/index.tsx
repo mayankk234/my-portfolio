6d12957d8fc3063bbdadea8ed560c736c746b5a6
Mayank <m.kashyap@sapalomera.cat>
2025-01-22 13:21:29 +0100

    add project

---
+++ pages/index.tsx
@@ -0,0 +1,40 @@
+import { Link } from "@heroui/link";
+import { Snippet } from "@heroui/snippet";
+import { Code } from "@heroui/code";
+import { button as buttonStyles } from "@heroui/theme";
+
+import { siteConfig } from "@/config/site";
+import { title, subtitle } from "@/components/primitives";
+import { GithubIcon } from "@/components/icons";
+import DefaultLayout from "@/layouts/default";
+
+export default function IndexPage() {
+  return (
+    <DefaultLayout>
+      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+        <div className="inline-block max-w-xl text-center justify-center">
+          <span className={title()}>Work&nbsp;</span>
+          <span className={title({ color: "violet" })}>In Progress&nbsp;</span>
+          <br />
+          <span className={title()}>
+            of the best portfolio you've ever seen.
+          </span>
+          <div className={subtitle({ class: "mt-4" })}>
+            Using Beautiful, fast and modern React UI library.
+          </div>
+        </div>
+
+        <div className="flex gap-3">
+          <Link
+            isExternal
+            className={buttonStyles({ variant: "bordered", radius: "full" })}
+            href={siteConfig.links.github}
+          >
+            <GithubIcon size={20} />
+            GitHub
+          </Link>
+        </div>
+      </section>
+    </DefaultLayout>
+  );
+}
